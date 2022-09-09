ceb00c621e2b8e3b2d8d9bb4a3318af11916fcbc
mrsamia <15203060@iubat.edu>
2022-09-09 16:18:36 +0600

    SRM | update

--- src/Components/PenIcon.js
+++ src/Components/PenIcon.js
@@ -7,31 +7,6 @@ function Pen_Icon(props) {
         <div className='cir'>
           <img src={pen_icon} className="pen_icon" alt="logo" />
           {/* <FaPen className="pen_icon "/> */}
-         
-          
-<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button>
-
-<div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div className="modal-dialog modal-dialog-centered" role="document">
-    <div className="modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div className="modal-body">
-      
-      </div>
-      <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
         </div>
     );
 }
