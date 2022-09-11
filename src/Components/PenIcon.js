fe0b4d24e259cc859da49b0dac258e80c7bf66c3
mrsamia <15203060@iubat.edu>
2022-09-11 23:44:48 +0600

    SRM | modal

--- src/Components/PenIcon.js
+++ src/Components/PenIcon.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import pen_icon from './Logos/pen_icon.svg';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 import { FaPen } from "react-icons/fa";
 import './components.css';
 function Pen_Icon(props) {
-    return (
-        <div className='cir'>
-          <img src={pen_icon} className="pen_icon" alt="logo" />
-          {/* <FaPen className="pen_icon "/> */}
-        </div>
-    );
+  const [modalShow, setModalShow] = useState(false)
+
+  function clickHandler() {
+    setModalShow(true)
+  }
+  function onHide() {
+    console.log("clicked")
+    setModalShow(false)
+  }
+  console.log(modalShow)
+  return (
+    <Fragment>
+      <div className='cir' onClick={clickHandler}>
+        <img src={pen_icon} className="pen_icon" alt="logo" />
+        {/* <FaPen className="pen_icon "/> */}
+      </div>
+      <Modal show={modalShow} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal>
+    </Fragment>
+  );
 }
 
 export default Pen_Icon;
