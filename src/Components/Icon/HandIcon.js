f3de7342bccbbd4be84f1120a879d4dab3c07b16
mrsamia <15203060@iubat.edu>
2022-09-21 14:07:43 +0600

    SRM | HandIconModal

--- src/Components/Icon/HandIcon.js
+++ src/Components/Icon/HandIcon.js
@@ -1,12 +1,33 @@
-import React from 'react';
+
 import '../components.css';
+import React, { Fragment, useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
 
 function HandIcon(props) {
-    return (
-        <div className='Hlcir'>
-              <img src={require("../Logos/thumb_icon.svg").default} className="hend_icon" alt="logo" />
-              
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (<Fragment>
+        <div className='Hlcir' onClick={handleShow}>
+            <img src={require("../Logos/thumb_icon.svg").default} className="hend_icon" alt="logo" />
+
         </div>
+        <Modal show={show} onHide={handleClose}>
+
+            <Modal.Body>
+                <ul className='ulPadding'>    
+                <ol className='ulPadding'>Just not interested</ol>
+                <ol className='ulPadding'>Job posted too long ago</ol>
+                <ol className='ulPadding'>Vague Description</ol>
+                <ol className='ulPadding'>Unrealistic Expectations</ol>
+                <ol className='ulPadding'>Too Many Applicants</ol>
+                <ol className='ulPadding'>Job posted too long ago</ol>
+                <ol className='ulPadding'> Poor reviews about the client</ol></ul>
+            </Modal.Body>
+
+        </Modal>
+        </Fragment>
     );
 }
 
